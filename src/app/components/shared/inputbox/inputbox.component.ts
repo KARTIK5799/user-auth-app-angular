073b2aeaa9988c665690d2f0ecd4c0b3a9298586
KARTIK5799 <thekartiksapkal@gmail.com>
2024-08-09 09:19:07 +0530

    changes

---
+++ src/app/components/shared/inputbox/inputbox.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inputbox',
+  standalone: true,
+  imports: [],
+  templateUrl: './inputbox.component.html',
+  styleUrl: './inputbox.component.css'
+})
+export class InputboxComponent {
+
+}
